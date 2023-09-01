d41fb534b7e23a30aec8cb969e22b02c8c4010d0
lepz0r <punkofthedeath@gmail.com>
2023-09-01 22:43:48 +0700

    Added debug feature

--- main.py
+++ main.py
@@ -6,37 +6,47 @@ import shutil
 import subprocess
 import pytz
 import psutil
+import logging
 from pathlib import Path
 from datetime import datetime
 from tzlocal import get_localzone
 
 
 def get_root_mountpoint():
+    root_mountpoint = "/run/samin/"
+    logging.debug("Mountpoint: " + root_mountpoint)
     return "/run/samin/"
 
 
 def get_confdir():
-    return get_root_mountpoint() + ".samin"
+    confdir = get_root_mountpoint() + ".samin"
+    logging.debug("Config directory: " + confdir)
+    return confdir
 
 
 def get_subvol_conf(subvol):
-    return get_confdir() + "/" + subvol + "/"
+    subvol_conf = get_confdir() + "/" + subvol + "/"
+    logging.debug("Subvolume config: " + subvol_conf)
+    return subvol_conf
 
 
 def generate_snapshot_metadata(desc):
     date = datetime.utcnow()
-    return {"date": date.strftime("%Y-%m-%dT%H:%M:%S"), "description": desc}
+    metadata = {"date": date.strftime("%Y-%m-%dT%H:%M:%S"), "description": desc}
+    return metadata
 
 
 def mnt(device, mountpoint):
     get_fs_command = ["blkid", "-o", "value", "-s", "TYPE", device]
     filesystem = subprocess.check_output(get_fs_command)
+    logging.debug("Filesystem of " + device + " is " + filesystem.decode("utf-8"))
 
     if filesystem != b"btrfs\n":
         raise Exception("Filesystem is not btrfs")
     try:
         if os.path.ismount(mountpoint) is False:
             mnt_command = ["mount", "-o", "subvolid=0", device, mountpoint]
+            logging.debug("Mounting filesystem")
             subprocess.check_call(mnt_command)
             return True
     except subprocess.CalledProcessError as e:
@@ -44,21 +54,24 @@ def mnt(device, mountpoint):
         return False
 
 
-def check_if_mounted(subvol):
+def check_if_subvol_mounted(subvol):
     partitions = psutil.disk_partitions()
     subvolid = btrfsutil.subvolume_info(subvol).id
 
     for i in partitions:
         if str(subvolid) in i.opts:
+            logging.debug("Subvolume " + subvol + " is mounted")
             return True
 
 
 def mount_root_mountpoint(root_mountpoint, device):
+    logging.debug("Creating mountpoint directory ")
     os.makedirs(root_mountpoint, exist_ok=True)
     mnt(device, root_mountpoint)
 
 
 def umnt(mountpoint):
+    logging.debug("Unmouting " + mountpoint)
     subprocess.check_call(["umount", mountpoint])
 
 
@@ -78,6 +91,7 @@ def get_snapshot_metadatas(subvol, device, utc=False):
                 subvol_confdir + "/snapshots/" + snapshot + "/metadata.json"
             ) as metadata:
                 current_metadata = json.loads(metadata.read())
+                logging.debug("Found snapshot metadata: " + str(current_metadata))
                 date_obj = datetime.strptime(
                     current_metadata["date"], "%Y-%m-%dT%H:%M:%S"
                 )
@@ -119,9 +133,12 @@ def get_next_snapshot_number(dir):
     snapshot_dir = os.listdir(path=dir)
 
     if len(snapshot_dir) == 0:
-        return "1"
+        number = "1"
     else:
-        return str(int(snapshot_dir[-1]) + 1)
+        number = str(int(snapshot_dir[-1]) + 1)
+
+    logging.debug("Next snapshot number: " + number)
+    return number
 
 
 def create_config(subvol, device):
@@ -132,8 +149,10 @@ def create_config(subvol, device):
     mount_root_mountpoint(root_mountpoint, device)
 
     if Path(confdir).exists() is False:
+        logging.debug("Creating samin config subvolume")
         btrfsutil.create_subvolume(confdir)
 
+    logging.debug("Creating subvolume config")
     os.makedirs(subvol_confdir + "/snapshots", exist_ok=True)
 
     umnt(root_mountpoint)
@@ -151,6 +170,7 @@ def delete_config(subvol, device):
         for snapshot_number in subvol_snapshots:
             delete_snapshot(subvol, device, snapshot_number, False)
 
+    logging.debug("Deleting config")
     shutil.rmtree(subvol_confdir)
 
     umnt(root_mountpoint)
@@ -162,9 +182,11 @@ def create_snapshot_dir(subvol, desc):
     metadata = generate_snapshot_metadata(desc)
     next_snapshot_number = get_next_snapshot_number(subvol_conf + "/snapshots")
     snapshot_dir = subvol_conf + "snapshots/" + next_snapshot_number
+    logging.debug("Creating snapshot dir at " + snapshot_dir)
     os.makedirs(snapshot_dir, exist_ok=True)
 
     with open(snapshot_dir + "/metadata.json", "w") as output:
+        logging.debug("Saving metadata: " + str(metadata))
         json.dump(metadata, output, indent=2)
 
     return snapshot_dir
@@ -176,6 +198,8 @@ def take_snapshot(subvol, device, desc="No description given"):
     mount_root_mountpoint(root_mountpoint, device)
 
     snapshot_dir = create_snapshot_dir(subvol, desc)
+
+    logging.debug("Taking snapshot at " + snapshot_dir + "/snapshot")
 
     btrfsutil.create_snapshot(
         root_mountpoint + subvol,
@@ -194,10 +218,31 @@ def rollback(subvol, device, snapshot_number):
 
     snapshot_dir = create_snapshot_dir(subvol, "rolled back to " + snapshot_number)
 
+    logging.debug(
+        "Moving old subvolume from "
+        + root_mountpoint
+        + subvol
+        + " to "
+        + snapshot_dir
+        + "/snapshot"
+    )
     os.rename(
         root_mountpoint + subvol,
         snapshot_dir + "/snapshot",
     )
+
+    logging.debug(
+        "Generating new snapshot from "
+        + subvol_conf
+        + "snapshots/"
+        + snapshot_number
+        + "/"
+        + "snapshot"
+        + " to "
+        + root_mountpoint
+        + subvol
+    )
+
     btrfsutil.create_snapshot(
         subvol_conf + "snapshots/" + snapshot_number + "/" + "snapshot",
         root_mountpoint + subvol,
@@ -232,18 +277,19 @@ def delete_snapshot(subvol, device, snapshot_numbers, no_mount_op=False):
     for snapshot in snapshots:
         snapshot_number = str(snapshot)
         if (
-            check_if_mounted(
+            check_if_subvol_mounted(
                 subvol_confdir + "snapshots/" + snapshot_number + "/" + "snapshot"
             )
             is True
         ):
-            print(
+            logging.error(
                 "Not removing snapshot #"
                 + snapshot_number
                 + ": snapshot is still mounted"
             )
 
         else:
+            logging.debug("Deleting snapshot: " + subvol_confdir + "snapshots/" + snapshot_number)
             btrfsutil.delete_subvolume(
                 subvol_confdir + "snapshots/" + snapshot_number + "/" + "snapshot"
             )
@@ -260,6 +306,7 @@ def parser():
 
     parser.add_argument("-s", "--subvolume", help="Specify subvolume", required=True)
     parser.add_argument("-b", "--device", help="Specify subvolume", required=True)
+    parser.add_argument("-d", "--debug", help="Enable debugging", action="store_true")
     parser.add_argument(
         "-u",
         "--utc",
@@ -272,7 +319,6 @@ def parser():
 
     t_snap_parser = subparser.add_parser("take-snapshot", help="Take a snapshot")
     t_snap_parser.add_argument(
-        "-d",
         "--description",
         help="Specify description",
         default="No description given",
@@ -287,6 +333,9 @@ def parser():
     l_snap_parser = subparser.add_parser("list-snapshots", help="List snapshots")
 
     args = parser.parse_args()
+
+    if args.debug is True:
+        logging.basicConfig(level=logging.DEBUG)
 
     if args.action == "create-config":
         create_config(args.subvolume, args.device)
